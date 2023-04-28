a38d244b6203d9d4fd8c28d326cb35bf404d9ccf
nehanarta <nehabaswanthrao@gmail.com>
2023-04-28 15:02:56 +0530

    Added:welcome to regex program

---
+++ src/Bridglabz/com/Regex.java
@@ -0,0 +1,12 @@
+package Bridglabz.com;
+import java.util.regex.*;
+
+public class Regex {
+    public static void main(String[] args) {
+        System.out.println("welcome to regex program");
+    }
+}
+
+
+
+
